c33d2d8d763434a2bf7bdd0ce25702a736d14015
Mr. Syntax <maam98617@gmail.com>
2025-12-31 02:48:12 +0330

    Update script.js

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ function updateWeeklyForecast(forecastData) {
     card.innerHTML = `
       <div class="day">${day}</div>
       <img src="https://openweathermap.org/img/wn/${data.icon}@2x.png">
-      <div class="temp">${Math.round(data.max)}° / ${Math.round(data.min)}°</div>
+      <div class="temp" dir="ltr">${Math.round(data.max)}° / ${Math.round(data.min)}°</div>
       <div class="desc">${data.desc}</div>
     `;
     container.appendChild(card);
