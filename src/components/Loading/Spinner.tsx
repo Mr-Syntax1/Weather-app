0004f0e368667acbea778ed62d460953cf01adc2
Green. Rose <maam98617@gmail.com>
2026-06-30 00:40:05 +0330

    Update Spinner.tsx

--- src/components/Loading/Spinner.tsx
+++ src/components/Loading/Spinner.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useLanguage } from '../../contexts/LanguageContext'; '../../contexts/LanguageContext'
 
 const Spinner: React.FC = () => {
+
+    const { language } = useLanguage()
+
     return (
         <div className="flex justify-center flex-col items-center py-8">
             <div className="animate-spin rounded-full h-14 w-14 border-4 border-blue-600 border-t-transparent mb-3"></div>
-            <p className='font-semibold'>...loading</p>
+            <p className='font-semibold'>{language === 'en' ? 'loading...' : '...loading'}</p>
         </div>
     );
 };
